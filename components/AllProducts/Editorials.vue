81a64c416cd7915a2fa4f1f589c946fc5e85e939
Kamolphon <kamolphon.tsw@mail.kmutt.ac.th>
2023-11-16 19:28:40 +0700

    full image screen

--- components/AllProducts/Editorials.vue
+++ components/AllProducts/Editorials.vue
@@ -5,14 +5,13 @@
         <p class="font-semibold">{{ product.category }}</p>
         <p>{{ product.description }}</p>
       </div>
-      <div class="flex items-center">
-        <div v-for="(productItem, index) in product.items" :key="index" class="flex items-center justify-center mx-4">
-          <div v-if="index === currentItemIndex" class="relative image-container">
-            <img :src="`images/${productItem.image}`" class="w-full h-full object-cover" />
-            <div class="absolute bottom-5 left-0 right-0 flex justify-center">
-              <div v-for="(item, i) in product.items" :key="i" class="h-2 w-2 rounded-full mx-1 bg-white"
-                :class="{ 'w-8': i === currentItemIndex }"></div>
-            </div>
+      <div v-for="(productItem, index) in product.items" :key="index" class="flex items-center justify-center">
+        <div class="relative image-container">
+          <img v-if="index === currentItemIndex" :src="`images/${productItem.image}`"
+            class="w-full h-full object-cover" />
+          <div class="absolute bottom-5 left-0 right-0 flex justify-center">
+            <div v-for="(item, i) in product.items" :key="i" class="h-2 w-2 rounded-full mx-1 bg-white"
+              :class="{ 'w-8': i === currentItemIndex }"></div>
           </div>
         </div>
       </div>
@@ -43,7 +42,6 @@ onMounted(() => {
 });
 </script>
   
-<style>
-/* Add any additional styles here */
+<style scoped>
 </style>
   
