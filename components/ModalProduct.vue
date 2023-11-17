9b14da2aaaac1dca8b9f0d7c38bdfa5675ee8f27
Kamolphon <kamolphon.tsw@mail.kmutt.ac.th>
2023-11-17 12:18:21 +0700

    set modal size

--- components/ModalProduct.vue
+++ components/ModalProduct.vue
@@ -14,7 +14,9 @@ const emits = defineEmits(['toggleModal'])
         <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity">
             <div class="fixed inset-0 z-10 flex items-center justify-center">
                 <div
-                    class="relative w-96 h-72 overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:w-full sm:max-w-lg">
+                    class="w-96 h-96 mx-3 md-size relative overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:w-full sm:max-w-lg">
+
+                    
                     <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                         <button type="button" @click="$emit('toggleModal')"
                             class="mt-3 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">Cancel</button>
@@ -24,9 +26,11 @@ const emits = defineEmits(['toggleModal'])
         </div>
     </div>
 </template>
-<style scoped></style>
-
-<!-- <div className="fixed text-zinc-700 inset-0 z-10 flex justify-center items-center bg-gray-100 bg-opacity-40 h-full modal">
-    <div className="lg:w-2/6 md:w-3/6 w-5/6 bg-white rounded-3xl shadow-2xl modal-content">
-        <div>
-            <div className='text-right my-3 mr-4 flex justify-between items-center mx-5'> -->
+<style scoped>
+@media (min-width: 768px) {
+    .md-size {
+        width: 546px;
+        height: 658px;
+    }
+}
+</style>
