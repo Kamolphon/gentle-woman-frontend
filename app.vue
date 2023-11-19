bb8ef7d9f6261d69d663345b5b42b5518886c1b2
Kamolphon <kamolphon.tsw@mail.kmutt.ac.th>
2023-11-19 10:15:06 +0700

    fix import

--- app.vue
+++ app.vue
@@ -1,11 +1,11 @@
 <script setup>
 import { getProducts } from "./composable/getProducts.js"
 import Navbar from "./components/layouts/Navbar.vue";
-import NewArrivalsVue from "./components/allProducts/NewArrivals.vue";
-import GentleWomanClub from "./components/allProducts/GentleWomanClub.vue";
-import Footer from "./components/layouts/Footer.vue";
-import Editorials from "./components/allProducts/Editorials.vue";
-import BackToTopBTN from "./components/BackToTopBTN.vue";
+import NewArrivalsVue from "@/components/allProducts/NewArrivals.vue";
+import GentleWomanClub from "@/components/allProducts/GentleWomanClub.vue";
+import Footer from "@/components/layouts/Footer.vue";
+import Editorials from "@/components/allProducts/Editorials.vue";
+import BackToTopBTN from "@/components/BackToTopBTN.vue";
 const products = await getProducts()
 
 </script>
