ef64ea2ab96d5cae6ca2881634cce09a4c1b6c22
Kamolphon <kamolphon.tsw@mail.kmutt.ac.th>
2023-11-19 09:50:55 +0700

    ignore env

--- composable/getProducts.js
+++ composable/getProducts.js
@@ -1,7 +1,6 @@
-const ROOT_API = import.meta.env.VITE_ROOT_API
 const getProducts = async () => {
     try{
-        const response = await fetch(`${ROOT_API}/Products`)
+        const response = await fetch(`https://gentle-woman-db.onrender.com/Products`)
         if (!response.ok) {
             throw new Error(`HTTP Error! Status: ${response.status}`);
         } else {
